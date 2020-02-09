ca6e370c083900ecbcd3be78e9c932c12e3afd01
Arun Reddy <arunreddy.kayathi@gmail.com>
2020-02-09 12:26:40 -0500

    Create structure and wired up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamList from "./components/StreamList/StreamList";
+import StreamCreate from "./components/StreamCreate/StreamCreate";
+import StreamEdit from "./components/StreamEdit/StreamEdit";
+import StreamDelete from "./components/StreamDelete/StreamDelete";
+import StreamShow from "./components/StreamShow/StreamShow";
+import Header from "./components/Header/Header";
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header></Header>
+        <Route path="/" exact component={StreamList}></Route>
+        <Route path="/streams/new" component={StreamCreate}></Route>
+        <Route path="/streams/edit" component={StreamEdit}></Route>
+        <Route path="/streams/delete" component={StreamDelete}></Route>
+        <Route path="/streams/show" component={StreamShow}></Route>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
